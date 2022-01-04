f2302ed9efe1cdf749aabc7b79b94a9a67896d2b
Jose Luis Blanco Claraco <joseluisblancoc@gmail.com>
2022-01-04 01:32:05 +0100

    kitti demo script

---
+++ scripts/kitti-run-seq.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+import glob, os
+import sys
+
+dir = sys.argv[1]
+cfg = sys.argv[2]
+print('Processing directory      : ' + dir)
+print('Using ICP pipeline config : ' + cfg)
+
+for i in range(0,1000):
+    f =  dir + '/{:06d}.mm'.format(i)
+    fnext =  dir + '/{:06d}.mm'.format(i+1)
+    cmd = 'mp2p-icp-run --input-global ' + f + ' --input-local ' + fnext + ' --config-filters-local ' + cfg + ' -c ' + cfg + ' -d'
+    print(cmd)
+    os.system(cmd)
