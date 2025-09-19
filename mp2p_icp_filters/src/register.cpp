1be13d43488cf376c59790b6f5c610a3061fb4be
Jose Luis Blanco-Claraco <joseluisblancoc@gmail.com>
2025-09-19 23:46:18 +0200

    Remove FilterDecimateVoxelsQuadratic

--- mp2p_icp_filters/src/register.cpp
+++ mp2p_icp_filters/src/register.cpp
@@ -31,7 +31,6 @@
 #include <mp2p_icp_filters/FilterCurvature.h>
 #include <mp2p_icp_filters/FilterDecimateAdaptive.h>
 #include <mp2p_icp_filters/FilterDecimateVoxels.h>
-#include <mp2p_icp_filters/FilterDecimateVoxelsQuadratic.h>
 #include <mp2p_icp_filters/FilterDeleteLayer.h>
 #include <mp2p_icp_filters/FilterDeskew.h>
 #include <mp2p_icp_filters/FilterEdgesPlanes.h>
@@ -64,7 +63,6 @@ MRPT_INITIALIZER(register_mola_lidar_segmentation)
     registerClass(CLASS_ID(mp2p_icp_filters::FilterCurvature));
     registerClass(CLASS_ID(mp2p_icp_filters::FilterDecimateAdaptive));
     registerClass(CLASS_ID(mp2p_icp_filters::FilterDecimateVoxels));
-    registerClass(CLASS_ID(mp2p_icp_filters::FilterDecimateVoxelsQuadratic));
     registerClass(CLASS_ID(mp2p_icp_filters::FilterDeleteLayer));
     registerClass(CLASS_ID(mp2p_icp_filters::FilterDeskew));
     registerClass(CLASS_ID(mp2p_icp_filters::FilterEdgesPlanes));
