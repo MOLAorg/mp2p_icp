5335af8e60f05fb4796e9bda73f4cc4be6961372
Jose Luis Blanco-Claraco <joseluisblancoc@gmail.com>
2025-09-30 09:28:05 +0200

    Update formatter script

--- scripts/formatter.sh
+++ scripts/formatter.sh
@@ -4,6 +4,7 @@ find \
     mp2p_icp \
     mp2p_icp_map \
     mp2p_icp_filters \
+    mp2p_icp_common \
     tests \
     -iname *.h -o -iname *.hpp -o -iname *.cpp -o -iname *.c \
     | xargs clang-format-14 -i
