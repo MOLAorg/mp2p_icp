94a4ebda1aea88756d499a33fc8aa5b7e67fa853
Jose Luis Blanco-Claraco <joseluisblancoc@gmail.com>
2025-10-10 12:28:53 +0200

    Add unit test for cov2cov optimizer

---
+++ tests/test-mp2p_deskew.cpp
@@ -0,0 +1,34 @@
+/*               _
+ _ __ ___   ___ | | __ _
+| '_ ` _ \ / _ \| |/ _` | Modular Optimization framework for
+| | | | | | (_) | | (_| | Localization and mApping (MOLA)
+|_| |_| |_|\___/|_|\__,_| https://github.com/MOLAorg/mola
+
+ A repertory of multi primitive-to-primitive (MP2P) ICP algorithms
+ and map building tools. mp2p_icp is part of MOLA.
+
+ Copyright (C) 2018-2025 Jose Luis Blanco, University of Almeria,
+                         and individual contributors.
+ SPDX-License-Identifier: BSD-3-Clause
+*/
+
+/**
+ * @file   test-mp2p_deskew.cpp
+ * @brief  Unit tests for matcher
+ * @author Jose Luis Blanco Claraco
+ * @date   Oct 10, 2025
+ */
+
+#include <mp2p_icp/metricmap.h>
+
+int main([[maybe_unused]] int argc, [[maybe_unused]] char** argv)
+{
+    try
+    {
+    }
+    catch (std::exception& e)
+    {
+        std::cerr << mrpt::exception_to_str(e) << "\n";
+        return 1;
+    }
+}
